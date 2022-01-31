771690d5c08142fbd9d4d03c642712b09f7406f9
allegroai <>
2022-01-31 12:39:39 +0200

    Fix ENV_API_DEFAULT_REQ_METHOD no default value causes ValueError if not specified

--- clearml_agent/backend_api/session/defs.py
+++ clearml_agent/backend_api/session/defs.py
@@ -26,4 +26,4 @@ POST requests can be used instead.
 
 However this has not been vigorously tested and may have unintended consequences.
 """
-ENV_API_DEFAULT_REQ_METHOD = EnvEntry("CLEARML_API_DEFAULT_REQ_METHOD")
+ENV_API_DEFAULT_REQ_METHOD = EnvEntry("CLEARML_API_DEFAULT_REQ_METHOD", default="GET")
